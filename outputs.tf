1b3304eea14a910d0f146d8aa635f2b45259122c
Wang, Dawei <dwang@entertainment.com>
2018-11-20 12:50:23 -0500

    output iam roles

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "ecs_cluster_arn" {
 output "arn" {
   value = "${aws_batch_compute_environment.managed.arn}"
 }
+
+output "ec2_instance_role" {
+  value = "${aws_iam_role.ecs_instance_role.arn}"
+}
+
+output "service_role" {
+  value = "${aws_iam_role.aws_batch_service_role}"
+}
