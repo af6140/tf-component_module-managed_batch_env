2fe25b77b432ea67ae54ee155a1a306b45991b83
Wang, Dawei <dwang@entertainment.com>
2018-11-20 15:03:48 -0500

    remove not needed outputs

--- outputs.tf
+++ outputs.tf
@@ -5,11 +5,3 @@ output "ecs_cluster_arn" {
 output "arn" {
   value = "${aws_batch_compute_environment.managed.arn}"
 }
-
-output "ec2_instance_role" {
-  value = "${aws_iam_role.ecs_instance_role.arn}"
-}
-
-output "service_role" {
-  value = "${aws_iam_role.aws_batch_service_role}"
-}
