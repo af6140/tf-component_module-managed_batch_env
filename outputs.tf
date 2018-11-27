4e46d71da8a82163b842f3a7b8aed6c969d65158
Wang, Dawei <dwang@entertainment.com>
2018-11-27 10:01:49 -0500

    variable not found error

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 locals {
-  ecs_cluster_arn = "${var.compute_resource_type == "EC2" ?  aws_batch_compute_environment.managed.0.ecs_cluster_arn : aws_batch_compute_environment.managed_spot.0.ecs_cluster_arn}"
-  arn             = "${var.compute_resource_type == "EC2" ? aws_batch_compute_environment.managed.0.arn : aws_batch_compute_environment.managed_spot.0.arn}"
+  ecs_cluster_arn = "${element(concat(aws_batch_compute_environment.managed.*.ecs_cluster_arn, aws_batch_compute_environment.managed_spot.*.ecs_cluster_arn),0)}"
+  arn             = "${element(concat(aws_batch_compute_environment.managed.*.arn ,aws_batch_compute_environment.managed_spot.*.arn),0)}"
 }
 
 output "ecs_cluster_arn" {
