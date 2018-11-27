f34bac89cc5719365d8ca9f563d781fa5e80a556
Wang, Dawei <dwang@entertainment.com>
2018-11-27 09:52:28 -0500

    missing quote

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,5 @@ output "ecs_cluster_arn" {
 }
 
 output "arn" {
-  value = "${var.compute_resource_type == EC2 ? aws_batch_compute_environment.managed.0.arn : aws_batch_compute_environment.managed_spot.0.arn}"
+  value = "${var.compute_resource_type == "EC2" ? aws_batch_compute_environment.managed.0.arn : aws_batch_compute_environment.managed_spot.0.arn}"
 }
