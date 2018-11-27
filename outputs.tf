b79eca7b0f1a01977448edc7afb58499068fdc75
Wang, Dawei <dwang@entertainment.com>
2018-11-27 09:49:15 -0500

    first

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "ecs_cluster_arn" {
-  value = "${var.compute_resource_type == "EC2" ?  aws_batch_compute_environment.managed.ecs_cluster_arn : aws_batch_compute_environment.managed_spot.ecs_cluster_arn}"
+  value = "${var.compute_resource_type == "EC2" ?  aws_batch_compute_environment.managed.0.ecs_cluster_arn : aws_batch_compute_environment.managed_spot.0.ecs_cluster_arn}"
 }
 
 output "arn" {
-  value = "${var.compute_resource_type == EC2 ? aws_batch_compute_environment.managed.arn : aws_batch_compute_environment.managed_spot.arn}"
+  value = "${var.compute_resource_type == EC2 ? aws_batch_compute_environment.managed.0.arn : aws_batch_compute_environment.managed_spot.0.arn}"
 }
